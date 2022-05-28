fbd8713d148c5c778a1464abd9e9d0897a09d5aa
student <student@itpark>
2022-05-28 19:06:13 +0300

    changed message, fixes #1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello home, classroom");
+        System.out.println("Hello from home, classroom");
     }
 }
